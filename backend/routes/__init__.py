3e0cbfdb6b66cc3a2cfc3d8984d428a8f8d77fb7
zach.bullard <zachary.r.bullard@gmail.com>
2025-04-22 09:32:15 -0400

    dockerize

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,4 +1,4 @@
-from backend.routes.auth import bp as auth_bp
-from backend.routes.jobs import bp as jobs_bp
+from .auth import bp as auth_bp
+from .jobs import bp as jobs_bp
 
 __all__ = ["auth_bp", "jobs_bp"]
