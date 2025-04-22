3d6d361c1077c25fd65b1ddaff7abe6ced482431
zach.bullard <zachary.r.bullard@gmail.com>
2025-04-22 08:56:17 -0400

    dockerize

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import find_packages, setup
 
 setup(
     name="jobpal",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "flask",
         "flask-sqlalchemy",
-        "flask-jwt-extended",
         "flask-migrate",
+        "flask-jwt-extended",
+        "flask-cors",
         "psycopg2-binary",
         "python-dotenv",
+        "gunicorn",
     ],
 )
